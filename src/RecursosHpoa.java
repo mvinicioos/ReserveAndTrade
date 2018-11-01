aaa4c24dcc317ba8cd7d1da5180902654a9d0518
mvinicioos <msvinicioos@gmail.com>
2018-11-01 15:26:07 -0300

    Os modelos criados (Frame, Dialog, Button, Label, TextField) tem o intuíto de facilitar a manutenção da interface, padronizar e ecnomizar código.
    Interface de cadastro de sala pronta. Basta apenas compilar para testar.
    
    *Corrigido problema de botões

--- src/RecursosHpoa.java
+++ src/RecursosHpoa.java
@@ -1,15 +1,13 @@
 import Model.UsuarioAdministrador;
-import View.CadastroSala;
-import View.ModeloDialog;
-import View.ModeloInterface;
-import View.ModeloPainel;
+import View.*;
 
 public class RecursosHpoa {
     public static void main(String[] args) {
         CadastroSala sala = new CadastroSala();
+        CadastroEquipamento usuario = new CadastroEquipamento();
         UsuarioAdministrador adm = new UsuarioAdministrador(1,"marcos", "silva", "msss@hot.com", "123",1);
 
-
+        //usuario.iniciar();
         sala.iniciar();
 
     }
