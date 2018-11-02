6d4940480fa1b9b857d2deed5e01fd645e2b1cd7
mvinicioos <msvinicioos@gmail.com>
2018-11-02 20:44:53 -0300

    ModeloCalendario pronto. Usuario fazendo busca por ID.
    Interface:
     - GerenciarRecursos:
         Busca apenas uma sala por id e exibe em uma label:
    
         Implementar:
            Fazer buscar e exibir um equipamento
            Criar uma instancia do calendario
            Botao para fazer uma reserva.

--- src/RecursosHpoa.java
+++ src/RecursosHpoa.java
@@ -1,18 +1,34 @@
 import Model.Equipamento;
 import Model.Reserva;
+import Model.Usuario;
 import Model.UsuarioAdministrador;
 import View.*;
+import com.toedter.calendar.JDateChooser;
+import com.toedter.calendar.JDayChooser;
+import sun.util.calendar.JulianCalendar;
+
+import java.util.Calendar;
 
 public class RecursosHpoa {
     public static void main(String[] args) {
         CadastroSala sala = new CadastroSala();
         CadastroEquipamento equip = new CadastroEquipamento();
-
+        GerenciarRecursos ger = new GerenciarRecursos();
         UsuarioAdministrador adm = new UsuarioAdministrador(1,"marcos", "silva", "msss@hot.com", "123",1);
         Equipamento equi = new Equipamento(1,"n","a","a");
 
+        //JulianCalendar calendario = new JulianCalendar();
 
+        //Calendar data = Calendar.getInstance();
+
+        
+
+
+        ger.exibirCaixaDialog();
         //sala.exibirCaixaDialog();
-        equip.exibirCaixaDialog();
+        //equip.exibirCaixaDialog();
+
+
+
     }
 }
